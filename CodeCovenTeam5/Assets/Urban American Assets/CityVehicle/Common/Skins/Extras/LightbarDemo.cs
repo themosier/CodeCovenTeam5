4c8452f5f7dd6cec552a501a7d9a155a1c814a5d
Domzom <Domcalderon@gmail.com>
2021-04-02 19:15:09 -0700

    Added Assets and Basic Scene

---
+++ CodeCovenTeam5/Assets/Urban American Assets/CityVehicle/Common/Skins/Extras/LightbarDemo.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightbarDemo : MonoBehaviour {
+    public List <Transform> rotor;
+    public List <GameObject> lights;
+    public bool Lightbar = false;
+    bool lightOn;
+    public int speed = 2;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!Lightbar)
+        {
+            foreach (GameObject Light in lights)
+            { Light.SetActive(false);
+                lightOn = false;
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Lightbar) {
+            if (!lightOn)
+            {
+                foreach (GameObject Light in lights)
+                { Light.SetActive(true);
+                    lightOn = true;
+                }
+            }
+            foreach (Transform Rotor in rotor) { Rotor.transform.Rotate(0,45* speed * Time.deltaTime, 0); } }
+        if (!Lightbar && lightOn)
+        {
+            foreach (GameObject Light in lights)
+            {
+                Light.SetActive(false);
+                lightOn = false;
+            }
+        }
+    }
+}
+  
+
