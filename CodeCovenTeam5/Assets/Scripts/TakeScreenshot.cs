4a3ed0a6120fd19499f00504b107d3866580ccf8
themosier <themosier11@gmail.com>
2021-04-02 18:51:23 -0700

    Made screenshot functionality

---
+++ CodeCovenTeam5/Assets/Scripts/TakeScreenshot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeScreenshot : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = gameObject.GetComponent<Camera>();
+        cam.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            cam.enabled = true;
+            Debug.Log("Space");
+            ScreenshotHandler.Screenshot_static(width, height);
+            //cam.enabled = false;
+        }
+    }
+}
