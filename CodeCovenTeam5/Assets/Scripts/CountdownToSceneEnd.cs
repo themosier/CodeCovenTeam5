4d9c11748f2ec49d94c2988e108fcce25fd609ed
Domzom <Domcalderon@gmail.com>
2021-04-07 11:20:57 -0700

    Added scene switcher to create an End state

---
+++ CodeCovenTeam5/Assets/Scripts/CountdownToSceneEnd.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CountdownToSceneEnd : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      StartCoroutine (loadSceneAfterDelay(5));  
+    }
+
+IEnumerator loadSceneAfterDelay(float waitbySecs){
+
+        yield return new WaitForSeconds(waitbySecs);
+
+         {
+   SceneManager.LoadScene(2);
+            Debug.Log("Switched to end scene");
+ }
+}
+}
