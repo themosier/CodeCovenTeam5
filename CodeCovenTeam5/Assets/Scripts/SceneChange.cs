57b2cbdfd7098c2bf156b0456527ff224fcd3586
Zsade <zsade.fleming3@gmail.com>
2021-04-07 14:11:33 -0400

    Core UI, Tutorial Overlay

---
+++ CodeCovenTeam5/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChange : MonoBehaviour
+{
+    public void GameStart()
+    {
+        SceneManager.LoadScene("Prologue");
+    }
+    public void PhotosNotesScreen()
+    {
+        SceneManager.LoadScene("PhotoAlbum");
+    }
+    public void CreditsScreen()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    public void MainMenuScreen()
+    {
+        SceneManager.LoadScene("Zsade");
+    }
+}
