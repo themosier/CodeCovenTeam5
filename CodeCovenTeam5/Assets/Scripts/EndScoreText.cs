a725baa3acd872c7445b024681fc41895ac096bf
Domzom <Domcalderon@gmail.com>
2021-04-07 13:15:07 -0700

    Fixed Epilogue

---
+++ CodeCovenTeam5/Assets/Scripts/EndScoreText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+ using UnityEngine;
+ using UnityEngine.UI;
+ 
+ public class EndScoreText : MonoBehaviour
+ {
+         public Text textbox;
+ 
+         void Start()
+         {
+                 textbox = GetComponent<Text>();
+         }
+ 
+         void Update()
+         {
+                 textbox.text = "Total Creatures Photographed: " + Score.playerScore;
+ 
+         }
+ }
